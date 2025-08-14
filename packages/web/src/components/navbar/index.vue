1275e63eaae742613d0caf551561b86eb084e42f
kvoon9 <a1176079417@qq.com>
2025-08-14 15:02:48 +0800

    perf: 优化页面布局

--- packages/web/src/components/navbar/index.vue
+++ packages/web/src/components/navbar/index.vue
@@ -53,23 +53,19 @@ function tryLogout() {
 </script>
 
 <template>
-  <div class="navbar">
-    <div class="left-side" space-x-2>
-      <a-space>
-        <img alt="logo" rounded-full size-10 src="/src/assets/logo.png">
-        <a-typography-title :style="{ margin: 0, fontSize: '18px' }" :heading="5" max-lg:hidden>
-          {{ t('project.name') }}
-        </a-typography-title>
-        <icon-menu-fold
-          v-if="!topMenu && appStore.device === 'mobile'" style="font-size: 22px; cursor: pointer"
-          @click="toggleDrawerMenu"
-        />
-      </a-space>
-      <a-space>
-        <a-tag>{{ version }}</a-tag>
-      </a-space>
-    </div>
-    <ul class="right-side">
+  <div flex="~ justify-between" border-b p4 pr10>
+    <a-space>
+      <img alt="logo" rounded-full size-10 src="/src/assets/logo.png">
+      <a-typography-title :style="{ margin: 0, fontSize: '18px' }" :heading="5" max-lg:hidden>
+        {{ t('project.name') }}
+      </a-typography-title>
+      <icon-menu-fold
+        v-if="!topMenu && appStore.device === 'mobile'" style="font-size: 22px; cursor: pointer"
+        @click="toggleDrawerMenu"
+      />
+      <a-tag>{{ version }}</a-tag>
+    </a-space>
+    <a-space size="large">
       <!-- <li>
         <a-tooltip :content="t('settings.language')">
           <a-button class="nav-btn" type="outline" shape="circle" @click="toggleLocales">
@@ -79,117 +75,57 @@ function tryLogout() {
           </a-button>
         </a-tooltip>
       </li> -->
-      <li>
-        <a-tooltip
-          :content="isFullscreen
-            ? t('settings.navbar.screen.toExit')
-            : t('settings.navbar.screen.toFull')
-          "
-        >
-          <a-button class="nav-btn" type="outline" shape="circle" @click="toggleFullScreen">
-            <template #icon>
-              <icon-fullscreen-exit v-if="isFullscreen" />
-              <icon-fullscreen v-else />
-            </template>
-          </a-button>
-        </a-tooltip>
-      </li>
-      <li>
-        <a-dropdown trigger="click" :popup-max-height="false">
-          <a-avatar hover:op75 cursor-pointer :image-url="selfInfo?.avatar">
-            <IconUser />
-          </a-avatar>
+      <a-tooltip
+        :content="isFullscreen
+          ? t('settings.navbar.screen.toExit')
+          : t('settings.navbar.screen.toFull')
+        "
+      >
+        <a-button class="nav-btn" type="outline" shape="circle" @click="toggleFullScreen">
+          <template #icon>
+            <icon-fullscreen-exit v-if="isFullscreen" />
+            <icon-fullscreen v-else />
+          </template>
+        </a-button>
+      </a-tooltip>
 
-          <template #content>
-            <div class="p4 flex-col gap-[15px] text-center">
-              <div v-if="selfInfo" class="flex flex-col gap-2">
-                <div class="text-lg text-primary font-semibold">
-                  {{ selfInfo.name }}
-                </div>
-                <div class="text-secondary text-sm">
-                  {{ t('weila-number') }}: {{ selfInfo.user_num }}
-                </div>
-                <div class="flex items-center gap-2">
-                  <a-tag color="blue">
-                    +{{ selfInfo.country_code }} {{ selfInfo.phone }}
-                  </a-tag>
-                </div>
+      <a-dropdown trigger="click" :popup-max-height="false">
+        <a-avatar hover:op75 cursor-pointer :image-url="selfInfo?.avatar">
+          <IconUser />
+        </a-avatar>
+
+        <template #content>
+          <div class="p4 flex-col gap-[15px] text-center">
+            <div v-if="selfInfo" class="flex flex-col gap-2">
+              <div class="text-lg text-primary font-semibold">
+                {{ selfInfo.name }}
               </div>
-              <div flex="~ col" mt6 gap2>
-                <a-button @click="bindingPhoneModalVisible = true">
-                  {{ t('binding-phone') }}
-                </a-button>
-                <a-button @click="resetPasswordModalVisible = true">
-                  {{ t('button.reset-password') }}
-                </a-button>
-                <a-button @click="tryLogout">
-                  {{ t('logout') }}
-                </a-button>
+              <div class="text-secondary text-sm">
+                {{ t('weila-number') }}: {{ selfInfo.user_num }}
+              </div>
+              <div class="flex items-center gap-2">
+                <a-tag color="blue">
+                  +{{ selfInfo.country_code }} {{ selfInfo.phone }}
+                </a-tag>
               </div>
             </div>
-          </template>
-        </a-dropdown>
-      </li>
-    </ul>
+            <div flex="~ col" mt6 gap2>
+              <a-button @click="bindingPhoneModalVisible = true">
+                {{ t('binding-phone') }}
+              </a-button>
+              <a-button @click="resetPasswordModalVisible = true">
+                {{ t('button.reset-password') }}
+              </a-button>
+              <a-button @click="tryLogout">
+                {{ t('logout') }}
+              </a-button>
+            </div>
+          </div>
+        </template>
+      </a-dropdown>
+    </a-space>
   </div>
 
   <ResetSelfPasswordModal v-model:open="resetPasswordModalVisible" />
   <BindingPhone v-model:open="bindingPhoneModalVisible" />
 </template>
-
-<style scoped lang="less">
-.navbar {
-  display: flex;
-  justify-content: space-between;
-  height: 100%;
-  background-color: var(--color-bg-2);
-  border-bottom: 1px solid var(--color-border);
-}
-
-.left-side {
-  display: flex;
-  align-items: center;
-  padding-left: 20px;
-}
-
-.center-side {
-  flex: 1;
-}
-
-.right-side {
-  display: flex;
-  padding-right: 20px;
-  list-style: none;
-
-  :deep(.locale-select) {
-    border-radius: 20px;
-  }
-
-  li {
-    display: flex;
-    align-items: center;
-    padding: 0 10px;
-  }
-
-  a {
-    color: var(--color-text-1);
-    text-decoration: none;
-  }
-
-  .nav-btn {
-    border-color: rgb(var(--gray-2));
-    color: rgb(var(--gray-8));
-    font-size: 16px;
-  }
-
-  .trigger-btn,
-  .ref-btn {
-    position: absolute;
-    bottom: 14px;
-  }
-
-  .trigger-btn {
-    margin-left: 14px;
-  }
-}
-</style>
