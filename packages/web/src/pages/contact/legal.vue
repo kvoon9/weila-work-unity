f4ec65e970499a145cf4759ec452ef4a5d360549
kvoon9 <a1176079417@qq.com>
2025-08-14 14:36:47 +0800

    chore: update

--- packages/web/src/pages/contact/legal.vue
+++ packages/web/src/pages/contact/legal.vue
@@ -107,198 +107,203 @@ async function uploadFile(option: any) {
 </script>
 
 <template>
-  <a-skeleton v-if="!isFetched" :animation="true">
-    <div p4 space-y-4>
-      <a-skeleton-line :rows="1" :widths="[120]" />
-      <a-skeleton-line :rows="1" :widths="['100%']" />
-      <a-skeleton-line :rows="1" :widths="['100%']" />
-      <a-skeleton-line :rows="1" :widths="[200]" />
+  <a-page-header
+    title="认证信息"
+    @back="$router.back"
+  >
+    <a-skeleton v-if="!isFetched" :animation="true">
+      <div p4 space-y-4>
+        <a-skeleton-line :rows="1" :widths="[120]" />
+        <a-skeleton-line :rows="1" :widths="['100%']" />
+        <a-skeleton-line :rows="1" :widths="['100%']" />
+        <a-skeleton-line :rows="1" :widths="[200]" />
+      </div>
+    </a-skeleton>
+    <div v-else>
+      <div p8 border-b>
+        <a-steps
+          :current="(() => {
+            if (!data)
+              return 1
+
+            if (data.state === 0)
+              return 2
+
+            if (data.state === 8 || data.state === 9)
+              return 3
+          })()"
+          w-300
+        >
+          <a-step title="填写信息" description="完善认证信息并提交" />
+          <a-step title="等待审核" description="我们将在1-3个工作日内完成审核" />
+          <a-step v-if="data?.state === 9" title="认证失败" description="审核未通过，请重新提交审核" />
+          <a-step v-else title="认证完成" description="审核通过，认证成功" />
+        </a-steps>
+      </div>
+
+      <div v-if="data" p4 border-b>
+        <a-descriptions
+          :title="data.category === 0 ? '企业认证' : '个人认证'" size="large"
+          bordered
+          :data="[
+            {
+              label: '审核状态',
+              value: data.state === 8
+                ? '已通过'
+                : data.state === 9
+                  ? '未通过'
+                  : '待审核',
+            },
+            {
+              label: '认证类别',
+              value: data.category === 0 ? '企业' : '个人',
+            },
+            {
+              label: '机构名称',
+              value: data.name,
+            },
+            {
+              label: '证件号码',
+              value: data.identify,
+            },
+            {
+              label: '更新时间',
+              value: data.updated,
+            },
+
+            {
+              label: '原因',
+              value: data?.reason || '无',
+            },
+          ]"
+        />
+      </div>
+
+      <div v-if="data?.category !== 0" p4 border-b>
+        <a-radio-group v-model="category" type="button">
+          <a-radio :value="0">
+            企业认证
+          </a-radio>
+          <a-radio :value="1">
+            个人认证
+          </a-radio>
+        </a-radio-group>
+      </div>
+      <div>
+        <a-form
+          v-if="category === 1"
+          p4
+          :model="userForm"
+          :rules="userRules"
+          border-b
+          @submit="submitUser"
+        >
+          <a-form-item label="姓名" field="name">
+            <a-input
+              v-model="userForm.name"
+              placeholder="请输入姓名"
+              allow-clear
+              style="width: 450px"
+            />
+          </a-form-item>
+
+          <a-form-item label="证件号码" field="identify">
+            <a-input
+              v-model="userForm.identify"
+              :max-length="18"
+              show-word-limit
+              placeholder="请输入 18 位身份证号"
+              allow-clear
+              style="width: 450px"
+            />
+          </a-form-item>
+
+          <a-form-item label="身份证正面" field="identify_card_front">
+            <a-upload
+              :multiple="false"
+              :limit="1"
+              :file-list="[{ url: userForm.identify_card_front }].filter(i => i.url)"
+              :default-file-list="[]"
+              list-type="picture-card"
+              image-preview
+              :custom-request="(options: any) => uploadFile(options).then(url => {
+                userForm.identify_card_front = url
+              })"
+            />
+          </a-form-item>
+
+          <a-form-item label="身份证反面" field="identify_card_reverse">
+            <a-upload
+              :multiple="false"
+              :file-list="[{ url: userForm.identify_card_reverse }].filter(i => i.url)"
+              :default-file-list="[]"
+              :limit="1"
+              list-type="picture-card"
+              image-preview
+              :custom-request="(options: any) => uploadFile(options).then(url => {
+                userForm.identify_card_reverse = url
+              })"
+            />
+          </a-form-item>
+
+          <!-- 提交按钮 -->
+          <a-form-item>
+            <a-button type="primary" size="large" html-type="submit">
+              更新认证信息
+            </a-button>
+          </a-form-item>
+        </a-form>
+
+        <a-form
+          v-if="category === 0"
+          p4
+          :model="corpForm"
+          :rules="corpRules"
+          border-b
+          @submit="submitCorp"
+        >
+          <a-form-item label="企业名称" field="name">
+            <a-input
+              v-model="corpForm.name"
+              placeholder="请输入企业名称"
+              allow-clear
+              style="width: 450px"
+            />
+          </a-form-item>
+
+          <a-form-item label="证件号码" field="identify">
+            <a-input
+              v-model="corpForm.identify"
+              :max-length="18"
+              show-word-limit
+              placeholder="请输入 18 位统一社会信用代码"
+              allow-clear
+              style="width: 450px"
+            />
+          </a-form-item>
+
+          <a-form-item label="营业执照" field="business_license">
+            <a-upload
+              :multiple="false"
+              :default-file-list="[]"
+              :file-list="[{ url: corpForm.business_license }].filter(i => i.url)"
+              :limit="1"
+              list-type="picture-card"
+              image-preview
+              :custom-request="(options: any) => uploadFile(options).then(url => {
+                corpForm.business_license = url
+              })"
+            />
+          </a-form-item>
+
+          <!-- 提交按钮 -->
+          <a-form-item>
+            <a-button type="primary" size="large" html-type="submit">
+              更新认证信息
+            </a-button>
+          </a-form-item>
+        </a-form>
+      </div>
     </div>
-  </a-skeleton>
-  <div v-else>
-    <div p8 border-b>
-      <a-steps
-        :current="(() => {
-          if (!data)
-            return 1
-
-          if (data.state === 0)
-            return 2
-
-          if (data.state === 8 || data.state === 9)
-            return 3
-        })()"
-        w-300
-      >
-        <a-step title="填写信息" description="完善认证信息并提交" />
-        <a-step title="等待审核" description="我们将在1-3个工作日内完成审核" />
-        <a-step v-if="data?.state === 9" title="认证失败" description="审核未通过，请重新提交审核" />
-        <a-step v-else title="认证完成" description="审核通过，认证成功" />
-      </a-steps>
-    </div>
-
-    <div v-if="data" p4 border-b>
-      <a-descriptions
-        :title="data.category === 0 ? '企业认证' : '个人认证'" size="large"
-        bordered
-        :data="[
-          {
-            label: '审核状态',
-            value: data.state === 8
-              ? '已通过'
-              : data.state === 9
-                ? '未通过'
-                : '待审核',
-          },
-          {
-            label: '认证类别',
-            value: data.category === 0 ? '企业' : '个人',
-          },
-          {
-            label: '机构名称',
-            value: data.name,
-          },
-          {
-            label: '证件号码',
-            value: data.identify,
-          },
-          {
-            label: '更新时间',
-            value: data.updated,
-          },
-
-          {
-            label: '原因',
-            value: data?.reason || '无',
-          },
-        ]"
-      />
-    </div>
-
-    <div v-if="data?.category !== 0" p4 border-b>
-      <a-radio-group v-model="category" type="button">
-        <a-radio :value="0">
-          企业认证
-        </a-radio>
-        <a-radio :value="1">
-          个人认证
-        </a-radio>
-      </a-radio-group>
-    </div>
-    <div>
-      <a-form
-        v-if="category === 1"
-        p4
-        :model="userForm"
-        :rules="userRules"
-        border-b
-        @submit="submitUser"
-      >
-        <a-form-item label="姓名" field="name">
-          <a-input
-            v-model="userForm.name"
-            placeholder="请输入姓名"
-            allow-clear
-            style="width: 450px"
-          />
-        </a-form-item>
-
-        <a-form-item label="证件号码" field="identify">
-          <a-input
-            v-model="userForm.identify"
-            :max-length="18"
-            show-word-limit
-            placeholder="请输入 18 位身份证号"
-            allow-clear
-            style="width: 450px"
-          />
-        </a-form-item>
-
-        <a-form-item label="身份证正面" field="identify_card_front">
-          <a-upload
-            :multiple="false"
-            :limit="1"
-            :file-list="[{ url: userForm.identify_card_front }].filter(i => i.url)"
-            :default-file-list="[]"
-            list-type="picture-card"
-            image-preview
-            :custom-request="(options: any) => uploadFile(options).then(url => {
-              userForm.identify_card_front = url
-            })"
-          />
-        </a-form-item>
-
-        <a-form-item label="身份证反面" field="identify_card_reverse">
-          <a-upload
-            :multiple="false"
-            :file-list="[{ url: userForm.identify_card_reverse }].filter(i => i.url)"
-            :default-file-list="[]"
-            :limit="1"
-            list-type="picture-card"
-            image-preview
-            :custom-request="(options: any) => uploadFile(options).then(url => {
-              userForm.identify_card_reverse = url
-            })"
-          />
-        </a-form-item>
-
-        <!-- 提交按钮 -->
-        <a-form-item>
-          <a-button type="primary" size="large" html-type="submit">
-            更新认证信息
-          </a-button>
-        </a-form-item>
-      </a-form>
-
-      <a-form
-        v-if="category === 0"
-        p4
-        :model="corpForm"
-        :rules="corpRules"
-        border-b
-        @submit="submitCorp"
-      >
-        <a-form-item label="企业名称" field="name">
-          <a-input
-            v-model="corpForm.name"
-            placeholder="请输入企业名称"
-            allow-clear
-            style="width: 450px"
-          />
-        </a-form-item>
-
-        <a-form-item label="证件号码" field="identify">
-          <a-input
-            v-model="corpForm.identify"
-            :max-length="18"
-            show-word-limit
-            placeholder="请输入 18 位统一社会信用代码"
-            allow-clear
-            style="width: 450px"
-          />
-        </a-form-item>
-
-        <a-form-item label="营业执照" field="business_license">
-          <a-upload
-            :multiple="false"
-            :default-file-list="[]"
-            :file-list="[{ url: corpForm.business_license }].filter(i => i.url)"
-            :limit="1"
-            list-type="picture-card"
-            image-preview
-            :custom-request="(options: any) => uploadFile(options).then(url => {
-              corpForm.business_license = url
-            })"
-          />
-        </a-form-item>
-
-        <!-- 提交按钮 -->
-        <a-form-item>
-          <a-button type="primary" size="large" html-type="submit">
-            更新认证信息
-          </a-button>
-        </a-form-item>
-      </a-form>
-    </div>
-  </div>
+  </a-page-header>
 </template>
